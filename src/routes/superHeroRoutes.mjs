16e983b243335e8e63b6aef939f4dcdfe106949a
Sayin Gonzalez <sayingonzalez@gmail.com>
2025-03-22 21:03:40 -0300

    Agregado: validaciones de reglas y errores

--- src/routes/superHeroRoutes.mjs
+++ src/routes/superHeroRoutes.mjs
@@ -10,6 +10,8 @@ import {
     eliminarSuperheroePorIdController,
     eliminarSuperheroePorNombreController
 } from '../controllers/superheroesController.mjs';
+import { validationDataSuperHeros } from '../middlewares/validationRules.mjs';
+import { handleValidationErrors } from '../middlewares/errorMiddleware.mjs';
 
 const router = express.Router();
 
@@ -18,10 +20,22 @@ router.get('/heroes/mayores-30', obtenerSuperheroesMayoresDe30Controller);
 router.get('/heroes/:id', obtenerSuperheroePorIdController);
 router.get('/heroes/buscar/poderes/:valor', obtenerSuperheroesPorPoderesController);
 router.get('/heroes/buscar/:atributo/:valor', buscarSuperheroesPorAtributoController);
-/* Spring 3 - TP1 */
-router.post('/heroes/crear', crearNuevoSuperheroeController);
-router.put('/heroes/actualizar/:id', actualizarSuperheroeController);
+/* Spring 3 - TP1 */ // Rutas agregadas
+/* Spring 3 - TP2 */ // Validaciones agregadas a las rutas para actualizar y eliminar heroes
+router.post('/heroes/crear', validationDataSuperHeros(), handleValidationErrors, crearNuevoSuperheroeController);
+router.put('/heroes/actualizar/:id', validationDataSuperHeros(), handleValidationErrors, actualizarSuperheroeController);
 router.delete('/heroes/eliminar/id/:id', eliminarSuperheroePorIdController);
 router.delete('/heroes/eliminar/nombre/:nombre', eliminarSuperheroePorNombreController);
 
 export default router;
+
+
+
+/* 
+
+                     función que valida que los         middleware
+              ruta - datos cumplan las reglas   -   Verifica si hubo errores  -    Controlador
+router.post('/ruta', registerValidationRules(),     handleValidationErrors,      funcionController)
+
+
+*/
